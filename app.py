4d2965f0bf68922e03a76cba20843faf75405b7f
Akash-052004 <akashsukumara05@gmail.com>
2025-10-20 17:02:36 +0530

    Fix lint spacing issue

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
 
-@app.route("/")
-def index():
-    return "Hello from final project!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello"
+
+
